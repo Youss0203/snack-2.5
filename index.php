57353faf4bc800a2af7191bc9888b339f05c7a2b
Youss0203 <youssefhalibi003@gmail.com>
2024-01-16 15:53:41 +0100

    correzione

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
-<?php
 
+<?php $average = [
+        "results" => 0
+    ];
 
-
-?>
+    if ( empty($_GET['a']) || empty($_GET['b'])){
+        echo 'NOPE';
+    } else {
+        // ? (a + b) / 2
+        $average['results'] = (floatval($_GET['a']) + floatval($_GET['b'])) / 2;
+    }
+    ?>
